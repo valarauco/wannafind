f1a949b8ac311b7fc9724292fdb5afd894d7b550
Manuel Delgado <manuel.delgado@ucr.ac.cr>
2017-05-26 11:28:00 -0600

    Add check for samba-vuln-cve-2017-7494

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wannafind
-Simple script using nmap to detect CVE-2017-0143 MS17-010 in your network
+Simple script using nmap to detect CVE-2017-0143 MS17-010 (Windows SMB) and CVE-2017-7494 (SAMBA) in your network
 
 ##  Usage:
 
--- wannafind.sh
+++ wannafind.sh
@@ -4,7 +4,7 @@
 
 which masscan &> /dev/null
 if [[ $? -ne 0 ]]; then
-  echo "mascan not found: apt install masscan ?"
+  echo "masscan not found: apt install masscan ?"
   exit 1 
 fi
 
@@ -16,18 +16,27 @@ fi
 
 subnet=$1
 tmp_subnet_list=$(mktemp)
-script_file="/usr/share/nmap/scripts/smb-vuln-ms17-010.nse"
+script_file_ms17_010="/usr/share/nmap/scripts/smb-vuln-ms17-010.nse"
+script_file_cve_2017_7494="/usr/share/nmap/scripts/samba-vuln-cve-2017-7494.nse"
 
-if [[ ! -f  "$script_file" ]] ; then
-  echo "Updating nmap scripts..."
-  sudo wget -O "$script_file" https://raw.githubusercontent.com/cldrn/nmap-nse-scripts/master/scripts/smb-vuln-ms17-010.nse
-  sudo chown root:root "$script_file"
+
+if [[ ! -f  "$script_file_ms17_010" ]] ; then
+  echo "Updating nmap scripts for ms17-010..."
+  sudo wget -O "$script_file_ms17_010" https://raw.githubusercontent.com/cldrn/nmap-nse-scripts/master/scripts/smb-vuln-ms17-010.nse
+  sudo chown root:root "$script_file_ms17_010"
+  sudo nmap --script-updatedb
+fi
+
+if [[ ! -f  "$script_file_cve_2017_7494" ]] ; then
+  echo "Updating nmap scripts for cve-2017-7494..."
+  sudo wget -O "$script_file_cve_2017_7494" https://gist.githubusercontent.com/wongwaituck/62c863ba7aa28a2d22d0fe9cbe14a18b/raw/056e217a955d8fff985a4dde001f9f937fa9c543/samba-vuln-cve-2017-7494.nse
+  sudo chown root:root "$script_file_cve_2017_7494"
   sudo nmap --script-updatedb
 fi
 
 echo "Scanning $subnet"
 sudo masscan -p445  $subnet > $tmp_subnet_list
-nmap -Pn -oA results -p445 --script smb-vuln-ms17-010 -iL $tmp_subnet_list | grep -B7 -A13 "VULNERABLE:"
+nmap -Pn -oA results -p445 --script smb-vuln-ms17-010 --script samba-vuln-cve-2017-7494 -iL $tmp_subnet_list | grep -B7 -A13 "VULNERABLE:"
 rm $tmp_subnet_list
 
 exit 0
